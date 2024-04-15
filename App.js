9af2e4657ac47b760e492f75abc01532c7434d15
Piyush Varshney <piyushvarshney54@gmail.com>
2024-04-15 21:51:33 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "hello"),
+    React.createElement("h2", {}, "hello2"),
+  ])
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
